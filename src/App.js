16c5939bdea909ca1d85564a1c301c7742a1b347
Zaidm <1337zain1337@mail.ru>
2023-05-23 19:23:01 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Todo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [],
+      newTask: ''
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({ newTask: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { newTask, tasks } = this.state;
+    if (newTask.trim() !== '') {
+      this.setState({
+        tasks: [...tasks, newTask],
+        newTask: ''
+      });
+    }
+  };
+
+  render() {
+    const { tasks, newTask } = this.state;
+
+    return (
+      <div>
+        <h1>Список задач (TODO)</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={newTask} onChange={this.handleChange} placeholder="Введите задачу" />
+          <button type="submit">Добавить</button>
+        </form>
+        <ul>
+          {tasks.map((task, index) => (
+            <li key={index}>{task}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Todo;
